1af8c24c14bc099f494d07d2b8e09d9c4d4a81dc
carlokruger <oUtME7tNxmfRnLxwgTxvUiUJ>
2020-09-01 14:22:07 +1200

    Initial set up

---
+++ creditcalc.py
@@ -0,0 +1,7 @@
+credit_principal = 'Credit principal: 1000'
+final_output = 'The credit has been repaid!'
+first_month = 'Month 1: repaid 250'
+second_month = 'Month 2: repaid 250'
+third_month = 'Month 3: repaid 500'
+
+# write your code here
